6fc80b05339d60d8c321877e099f02fd9281f651
MadFox <madfox@laserflare.net>
2025-02-04 02:11:30 +0400

    feat: add polkit svc

--- services.nix
+++ services.nix
@@ -1,13 +1,27 @@
 {pkgs, ...}: {
-
-  systemd.user.services.gammastepx = {
-    description = "Gammastep";
-    wantedBy = [ "graphical-session.target" ];
-    after = [ "graphical-session-pre.target" ];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.gammastep}/bin/gammastep -v -b 1.0:0.85 -t 4500:2500 -m wayland";
-      Restart = "always";
+  systemd.user.services = {
+    gammastepx = {
+      description = "Gammastep";
+      wantedBy = [ "graphical-session.target" ];
+      after = [ "graphical-session-pre.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.gammastep}/bin/gammastep -v -b 1.0:0.85 -t 4500:2500 -m wayland";
+        Restart = "always";
+      };
+    };
+    polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+        };
     };
   };
 }
