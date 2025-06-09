4f4f067a62eb55d59c69b98f00f85df6a9164b7d
MadFox <madfox@laserflare.net>
2025-06-10 02:37:42 +0400

    feat: add community cache instances

--- baseconf.nix
+++ baseconf.nix
@@ -29,7 +29,11 @@
       ];
       substituters = [
         "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
       ];
+      trusted-substituters = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
     };
     distributedBuilds = true;
   };
