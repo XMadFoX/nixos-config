a832e512abaf41b2be2a56c9acaed6b279ed755f
MadFox <madfox@laserflare.net>
2025-10-23 23:57:02 +0400

    feat: configure xdg portals for niri on gvino

--- hosts/gvino/configuration.nix
+++ hosts/gvino/configuration.nix
@@ -58,6 +58,24 @@
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
+  xdg = {
+    portal = {
+      enable = true;
+      config.niri = {
+         default = ["gnome" "gtk"];
+            "org.freedesktop.impl.portal.Access" = "gtk";
+            "org.freedesktop.impl.portal.FileChooser" = "gtk";
+            "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+            "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
+      };
+      extraPortals = with pkgs; [
+         xdg-desktop-portal-gnome
+         xdg-desktop-portal-gtk
+         xdg-desktop-portal-wlr
+      ];
+    };
+  };
+
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = false;
   services.desktopManager.plasma6.enable = true;
