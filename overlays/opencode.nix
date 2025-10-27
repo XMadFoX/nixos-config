838fb8a5c4038fedab38934de1a89d682b7b0836
MadFox <madfox@laserflare.net>
2025-10-27 22:47:23 +0400

    feat(opencode): bump version

--- overlays/opencode.nix
+++ overlays/opencode.nix
@@ -1,6 +1,6 @@
 final: prev:
 let
-  version = "0.13.5";
+  version = "0.15.14";
 in
 {
   opencode = prev.opencode.overrideAttrs (old: rec {
@@ -10,15 +10,15 @@ in
       owner = "sst";
       repo = "opencode";
       tag = "v${version}";
-      sha256 = "sha256-GiByJg4NpllA4N4QGSyWsBNqKqKIdxicIjQpc7mHgEs=";
+      sha256 = "sha256-K7TmsJm11uDNjN3fUaapM1A01FmHUSfXMiqOzhLzRI8=";
     };
 
     tui = old.tui.overrideAttrs (_: {
-      vendorHash = "sha256-H+TybeyyHTbhvTye0PCDcsWkcN8M34EJ2ddxyXEJkZI=";
+      vendorHash = "sha256-g3+2q7yRaM6BgIs5oIXz/u7B84ZMMjnxXpvFpqDePU4=";
     });
 
     node_modules = old.node_modules.overrideAttrs (_: {
-      outputHash = "sha256-fGf2VldMlxbr9pb3B6zVL+fW1S8bRjefJW+jliTO73A=";
+      outputHash = "sha256-8pJBLNPuF7+wcUCNoI9z68q5Pl6Mvm1ZvIDianLPdHo=";
     });
   });
 }
