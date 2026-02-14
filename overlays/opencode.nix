3cd1d93e77a04d3ee0254c27bcf280d5e740e7de
MadFox <madfox@laserflare.net>
2026-02-14 23:19:45 +0400

    feat: update opencode version & override

--- overlays/opencode.nix
+++ overlays/opencode.nix
@@ -1,24 +1,28 @@
 final: prev:
 let
-  version = "0.15.14";
+  version = "1.1.51";
 in
 {
-  opencode = prev.opencode.overrideAttrs (old: rec {
-    inherit version;
-
-    src = prev.fetchFromGitHub {
-      owner = "sst";
-      repo = "opencode";
-      tag = "v${version}";
-      sha256 = "sha256-K7TmsJm11uDNjN3fUaapM1A01FmHUSfXMiqOzhLzRI8=";
+  opencode =
+    (prev.opencode.overrideAttrs (old: {
+      version = version;
+    }))
+    // {
+      passthru = (prev.opencode.passthru or { }) // {
+        sources = {
+          "aarch64-linux" = prev.fetchurl {
+            url = "https://github.com/sst/opencode/releases/download/v${version}/opencode-linux-arm64.zip";
+            hash = "sha256-v3j0oL2WL4DezOxdEKgOq4f8BkdR01zlYHUzbUUjgtEA=";
+          };
+          "x86_64-darwin" = prev.fetchurl {
+            url = "https://github.com/sst/opencode/releases/download/v${version}/opencode-darwin-x64.zip";
+            hash = "sha256-+kycX2mbCy7p6VOKO9HequCoJi/5nT8Pf09obZEuj1A=";
+          };
+          "x86_64-linux" = prev.fetchurl {
+            url = "https://github.com/sst/opencode/releases/download/v${version}/opencode-linux-x64.zip";
+            hash = "sha256-ca919c8b64f4a63993df70a47f383751478e8e3985c275c5d49e46a217ab97f9";
+          };
+        };
+      };
     };
-
-    tui = old.tui.overrideAttrs (_: {
-      vendorHash = "sha256-g3+2q7yRaM6BgIs5oIXz/u7B84ZMMjnxXpvFpqDePU4=";
-    });
-
-    node_modules = old.node_modules.overrideAttrs (_: {
-      outputHash = "sha256-8pJBLNPuF7+wcUCNoI9z68q5Pl6Mvm1ZvIDianLPdHo=";
-    });
-  });
 }
