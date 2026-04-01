096935eca4d7215b473fe783255bdbcdebb24578
MadFox <madfox@laserflare.net>
2026-04-01 23:38:10 +0400

    chore(opencode): bump version

--- overlays/opencode.nix
+++ overlays/opencode.nix
@@ -1,28 +1,30 @@
 final: prev:
 let
-  version = "1.1.51";
+  version = "1.2.26";
 in
 {
-  opencode =
-    (prev.opencode.overrideAttrs (old: {
-      version = version;
-    }))
-    // {
-      passthru = (prev.opencode.passthru or { }) // {
-        sources = {
-          "aarch64-linux" = prev.fetchurl {
-            url = "https://github.com/sst/opencode/releases/download/v${version}/opencode-linux-arm64.zip";
-            hash = "sha256-v3j0oL2WL4DezOxdEKgOq4f8BkdR01zlYHUzbUUjgtEA=";
-          };
-          "x86_64-darwin" = prev.fetchurl {
-            url = "https://github.com/sst/opencode/releases/download/v${version}/opencode-darwin-x64.zip";
-            hash = "sha256-+kycX2mbCy7p6VOKO9HequCoJi/5nT8Pf09obZEuj1A=";
-          };
-          "x86_64-linux" = prev.fetchurl {
-            url = "https://github.com/sst/opencode/releases/download/v${version}/opencode-linux-x64.zip";
-            hash = "sha256-ca919c8b64f4a63993df70a47f383751478e8e3985c275c5d49e46a217ab97f9";
-          };
-        };
+  opencode = prev.opencode.overrideAttrs (
+    old:
+    let
+      src = prev.fetchFromGitHub {
+        owner = "anomalyco";
+        repo = "opencode";
+        tag = "v${version}";
+        name = "opencode-${version}-source";
+        hash = "sha256-+bQEfrqv9tAmXUMcvyUM0hJGpXgt09IWoKYt8I/jBlU=";
       };
-    };
+    in
+    {
+      inherit version src;
+
+      node_modules = old.node_modules.overrideAttrs (_: {
+        inherit version src;
+        outputHash =
+          if prev.stdenvNoCC.hostPlatform.isDarwin then
+            "sha256-KlE4U87sVWoB2eXngUU7w+Z3F7oqh3NPhoCRHQqQm1s="
+          else
+            "sha256-byKXLpfvidfKl8PshUsW0grrRYRoVAYYlid0N6/ke2c=";
+      });
+    }
+  );
 }
