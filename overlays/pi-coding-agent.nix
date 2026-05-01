f73d3146baa66e52868dc0f09027bcc4636508a9
MadFox <madfox@laserflare.net>
2026-05-01 23:58:35 +0400

    chore(pi-agent): bump to 0.71.1

--- overlays/pi-coding-agent.nix
+++ overlays/pi-coding-agent.nix
@@ -1,13 +1,13 @@
 final: prev:
 let
-  version = "0.70.6";
+  version = "0.71.1";
   src = prev.fetchFromGitHub {
     owner = "badlogic";
     repo = "pi-mono";
-    tag = "v${version}";
-    hash = "sha256-XZUnKk+B9kWn51kRfMkfInYCz+5hVuWQBvgOm9PO9bo=";
+    rev = "80a439055d467001279d711c9b5d737df35113e0";
+    hash = "sha256-FOR0py2stVmRwdeMr7Oh6xwYrlcyUWE9f0OEKF2rO5g=";
   };
-  npmDepsHash = "sha256-pEVIqp9rbuHFE6eqSmADmIXWAPey1VbD7qmOJwksz1o=";
+  npmDepsHash = "sha256-irLlmq/to4x0GnNhSFVmfiuaiPx3B9l+PhlVeJSfhpU=";
 in
 {
   pi-coding-agent = prev.pi-coding-agent.overrideAttrs (_old: {
