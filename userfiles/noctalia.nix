538d2ee10b9ab67eb7f6b4821a393bad0fd1381d
MadFox <madfox@laserflare.net>
2026-05-03 03:14:51 +0400

    feat(noctalia): improve settings, include battery only on tsiteli

--- userfiles/noctalia.nix
+++ userfiles/noctalia.nix
@@ -1,50 +1,74 @@
+{ lib, osConfig, ... }:
+
+let
+  isTsiteli = osConfig.networking.hostName == "tsiteli";
+
+  rightWidgets = [
+    { id = "Network"; }
+    { id = "Bluetooth"; }
+    { id = "SystemMonitor"; }
+    {
+      id = "ControlCenter";
+      useDistroLogo = true;
+    }
+  ];
+in
 {
   programs.noctalia-shell = {
     enable = true;
-    settings = {
-      bar = {
-        density = "compact";
-        position = "top";
-        widgets = {
-          left = [
-            {
-              id = "ControlCenter";
-              useDistroLogo = true;
-            }
-            { id = "Network"; }
-            { id = "Bluetooth"; }
-          ];
-          center = [
-            {
-              id = "Workspace";
-              hideUnoccupied = false;
-              labelMode = "none";
-            }
-          ];
-          right = [
+    settings = lib.mkMerge [
+      {
+        bar = {
+          density = "compact";
+          position = "top";
+          widgets = {
+            left = [
+              {
+                id = "Workspace";
+                hideUnoccupied = false;
+                labelMode = "none";
+              }
+              { id = "ActiveWindow"; }
+            ];
+            center = [
+              {
+                id = "Clock";
+                formatHorizontal = "HH:mm";
+                useMonospacedFont = true;
+                usePrimaryColor = true;
+              }
+              { id = "Volume"; }
+            ];
+            right = rightWidgets;
+          };
+        };
+        # colorSchemes.predefinedScheme = "";
+        general = {
+          avatarImage = "/home/madfox/.face";
+          radiusRatio = 0.4;
+        };
+        location = {
+          monthBeforeDay = false;
+          name = "Tbilisi, Georgia";
+        };
+        wallpaper = {
+          # managed externaly, dont let noctalia override
+          enabled = false;
+        };
+      }
+
+      (lib.mkIf isTsiteli {
+        bar.widgets.right = lib.mkForce (
+          [
             {
               id = "Battery";
               alwaysShowPercentage = false;
               warningThreshold = 30;
             }
-            {
-              id = "Clock";
-              formatHorizontal = "HH:mm";
-              useMonospacedFont = true;
-              usePrimaryColor = true;
-            }
-          ];
-        };
-      };
-      # colorSchemes.predefinedScheme = "";
-      general = {
-        avatarImage = "/home/madfox/.face";
-        radiusRatio = 0.4;
-      };
-      location = {
-        monthBeforeDay = false;
-        name = "Tbilisi, Georgia";
-      };
-    };
+          ]
+          ++ rightWidgets
+        );
+      })
+    ];
   };
 }
